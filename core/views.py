15959c20131faa1b7c206beb241f8dcf3f3c7bff
josue23 <josuerodrigues@gmail.com>
2017-08-15 06:34:30 -0300

    Sistema de templates(Template tags, Variáveis e filtros)

--- core/views.py
+++ core/views.py
@@ -4,4 +4,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse('Hello World')
+	texts = ['Lorem ipsum dolor sit amet', 'consectetur adipisicing elit']
+	context = {
+	'title': 'django e-commerce',
+	'texts': texts
+	}
+	return render(request, 'index.html', context)
+	#return HttpResponse('Hello World')
